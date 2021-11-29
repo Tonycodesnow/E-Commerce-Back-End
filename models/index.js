87d8ba04bf1c1e8e789f93ed0b28837e9d3e86bc
Ant <huertinez.family@outlook.com>
2021-11-28 23:31:09 -0800

    completed category routes and index models

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-    foreignKey: "product_id"
+    foreignKey: "category_id"
 });
 
 // Categories have many Products
@@ -16,16 +16,12 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  as: 'tags',
-  foreignKey: "product_tag",
+  through:ProductTag
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  as: 'Products',
-  foreignKey: "tag_id",
+  through:ProductTag,
 });
 
 module.exports = {
