c6494041854694599ecb210fc4392b9256db40c1
Ant <huertinez.family@outlook.com>
2021-12-04 13:43:03 -0800

    completed readme

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# E-commerce Back End Starter Code
+# E-commerce Back End
+
+<br />
+
+![GitHub license](https://img.shields.io/badge/license-MIT-55002b.svg) <br />
+
+## Table of Contents 
+
+- [Description](#description)
+- [Installation](#installation)
+- [Screenshots](#screenshots)
+- [Demos](#demos)
+- [Deployed](#deployedapplicationlink)
+- [Languages](#languages)
+- [License](#license)
+- [Questions](#questions)
+
+<br />
+<br />
+
+## Description
+
+Due to the prevalence of Internet retail, also known as e-commerce, is the largest sector of the electronics industry, having generated an estimated US$29 trillion in 2017 (Source: United Nations Conference on Trade and Development). This application is the back end for an e-commerce site using Express.js API and configured to use Sequelize to interact with a MySQL database.<br />
+
+## Installation
+To install all the dependencies, use the command below:
+```
+git clone git@github.com:Tonycodesnow/E-Commerce-Back-End.git
+```
+Then, inside the folder, run the following command:
+```
+npm init
+```
+Then, install the dependencies with command:
+```
+npm i
+```
+Then, run the folloeing command to run the seed file:
+```
+npm run seed
+```
+When you're ready to run the application, from the root folder, type the below into your command line to start the server:
+```
+npm start
+```
+Once the E-commerce application runs, the user can utilize the functionality of the crud methods of GET, POST, PUT, and DELETING data while being tested in Insomnia <br />
+
+## Screenshots
+
+screenshot in insomia
+
+<p align="center"><img src="./dist/media/livesite.png" width="45%"></p> <br /> 
+
+## Demos
+
+Click on the link below for a video demonstration of the application and the generated API routes:
+
+https://www.youtube.com/watch?v=2_dT2A-LKOg
+
+
+## Github application link
+
+https://github.com/Tonycodesnow/E-Commerce-Back-End  <br />
+
+## Languages
+
+JavaScript, Node, Inquirer, MySQL2, Sequelize, Express.js API and the dotenv package. <br />
+
+
+## License
+
+This project is licensed under the MIT license. <br />
+  
+<br />
+
+## Questions?
+
+If you have any questions, contact me at: 
+tony.huertinez@gmail.com  <br />
+Check out my other work at GitHUb:
+[antoniohuerta](https://github.com/Tonycodesnow) <br />
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,38 +1,105 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  
+router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
-
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
-
-router.post('/', (req, res) => {
-  // create a new tag
-  Tag.create({
-    name: req.body.name
+  Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock"],
+      },
+    ],
   })
-    .then(tag => {
-      res.json(tag);
-    })
-    .catch(err => {
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.get("/:id", (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock"],
+      },
+    ],
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.post("/", (req, res) => {
+  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((tag) => {
+      res.json(tag);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
+      res.json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err); // 500 = internal server error
+    });
+});
+
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      if (!tag) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
+      res.json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err); // 500 = internal server error
+    });
 });
 
 module.exports = router;
